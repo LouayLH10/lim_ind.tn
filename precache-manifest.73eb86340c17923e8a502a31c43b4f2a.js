360d7aaa24a491125a335db35527b838bd06805b
Hassine Louay <104630080+LouayLH10@users.noreply.github.com>
2025-03-01 16:25:26 +0100

    bild

---
+++ precache-manifest.73eb86340c17923e8a502a31c43b4f2a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282a31e328e00c46399eb1f51f799cef",
+    "url": "/louayind_fron/index.html"
+  },
+  {
+    "revision": "3eb686d6eb9525e5d8c7",
+    "url": "/louayind_fron/static/css/2.94c5516e.chunk.css"
+  },
+  {
+    "revision": "c27e0c9ea9058ae91844",
+    "url": "/louayind_fron/static/css/main.2e9a8569.chunk.css"
+  },
+  {
+    "revision": "3eb686d6eb9525e5d8c7",
+    "url": "/louayind_fron/static/js/2.77878fec.chunk.js"
+  },
+  {
+    "revision": "c27e0c9ea9058ae91844",
+    "url": "/louayind_fron/static/js/main.0fb63f78.chunk.js"
+  },
+  {
+    "revision": "b34ad61cc1e47425018c",
+    "url": "/louayind_fron/static/js/runtime~main.97b52de9.js"
+  },
+  {
+    "revision": "cb02ed21772b8f673f8b64f33cd414e9",
+    "url": "/louayind_fron/static/media/448948920_2351813105024380_8089309549687949757_n.cb02ed21.jpg"
+  },
+  {
+    "revision": "00b0f8d1e1d8a309862a7893191ed505",
+    "url": "/louayind_fron/static/media/448950463_1195122918309938_6478207158724499985_n.00b0f8d1.jpg"
+  },
+  {
+    "revision": "344a7b4a0c98289af7e5809571c5eba8",
+    "url": "/louayind_fron/static/media/449176808_2055772204816609_4323812945297030382_n.344a7b4a.jpg"
+  },
+  {
+    "revision": "e3f4adbb2fe37b4ca1b8a3580159db7c",
+    "url": "/louayind_fron/static/media/449626188_444195275239702_4730601301765446573_n.e3f4adbb.jpg"
+  },
+  {
+    "revision": "d2e721a4981c3fd136d8e4ea592223bb",
+    "url": "/louayind_fron/static/media/449665207_783344360307700_5871136384043032416_n.d2e721a4.jpg"
+  },
+  {
+    "revision": "b4891719b36d6c9b228c22f2ae9c0163",
+    "url": "/louayind_fron/static/media/Cheminée 018.b4891719.jpg"
+  },
+  {
+    "revision": "60506e766a360497229f59ebc7be974a",
+    "url": "/louayind_fron/static/media/Logo-norme-ISO-14001.60506e76.png"
+  },
+  {
+    "revision": "badaea5f09291b57e66f55475a30fac9",
+    "url": "/louayind_fron/static/media/Logo-norme-ISO-45001.badaea5f.png"
+  },
+  {
+    "revision": "15d54d142da2f2d6f2e90ed1d55121af",
+    "url": "/louayind_fron/static/media/fa-brands-400.15d54d14.ttf"
+  },
+  {
+    "revision": "cbcf42b2e9228a8f5bad42717d8a88db",
+    "url": "/louayind_fron/static/media/fa-brands-400.cbcf42b2.woff2"
+  },
+  {
+    "revision": "262525e2081311609d1fdab966c82bfc",
+    "url": "/louayind_fron/static/media/fa-regular-400.262525e2.ttf"
+  },
+  {
+    "revision": "89672701a5874b80be27649e0494e354",
+    "url": "/louayind_fron/static/media/fa-regular-400.89672701.woff2"
+  },
+  {
+    "revision": "269f971cec0d5dc864fe9ae080b19e23",
+    "url": "/louayind_fron/static/media/fa-solid-900.269f971c.ttf"
+  },
+  {
+    "revision": "4a6591ab5460ae5cbff1ecbd6e52193a",
+    "url": "/louayind_fron/static/media/fa-solid-900.4a6591ab.woff2"
+  },
+  {
+    "revision": "4ed293ceaca9b5b2d9cd74a477963fae",
+    "url": "/louayind_fron/static/media/fa-v4compatibility.4ed293ce.ttf"
+  },
+  {
+    "revision": "a772da7bff216cd36214bc44165bba3e",
+    "url": "/louayind_fron/static/media/fa-v4compatibility.a772da7b.woff2"
+  },
+  {
+    "revision": "131af536c238217dbad1de6a4a778506",
+    "url": "/louayind_fron/static/media/industrial.131af536.png"
+  },
+  {
+    "revision": "f566f414aa1d3cbdf44b20901ee18682",
+    "url": "/louayind_fron/static/media/logo_original.f566f414.png"
+  },
+  {
+    "revision": "1a71684701588761fa20c66579e57cc8",
+    "url": "/louayind_fron/static/media/maison.1a716847.png"
+  },
+  {
+    "revision": "0d412faccf9d4fb292615b2ec520e587",
+    "url": "/louayind_fron/static/media/ouvrier-dans-usine.0d412fac.jpg"
+  },
+  {
+    "revision": "8b80f6e72a343ad9390035ffda518d64",
+    "url": "/louayind_fron/static/media/personne-combinaison-protection-travaillant-dans-centrale-nucleaire.8b80f6e7.jpg"
+  },
+  {
+    "revision": "f27e3f56d80a48c3a4832986da5353d9",
+    "url": "/louayind_fron/static/media/png-transparent-organization-iso-9000-iso-9001-2015-certification-iso-9001-text-trademark-logo-thumbnail.f27e3f56.png"
+  },
+  {
+    "revision": "3593bd5792564844cd8d2f636964c83b",
+    "url": "/louayind_fron/static/media/portrait-ingenieur-chantier-pendant-heures-travail.3593bd57.jpg"
+  },
+  {
+    "revision": "28c736345c19380f1e11cc9bb2a110b1",
+    "url": "/louayind_fron/static/media/portrait-jeune-travailleur-dans-casque-dans-grande-usine-metallurgie.28c73634.jpg"
+  },
+  {
+    "revision": "10284267ce8291b7ed95633bb7c042f4",
+    "url": "/louayind_fron/static/media/reservoir.10284267.png"
+  },
+  {
+    "revision": "accd2e7d8ff519da3307a78087600b05",
+    "url": "/louayind_fron/static/media/tuyauterie.accd2e7d.png"
+  }
+]);
