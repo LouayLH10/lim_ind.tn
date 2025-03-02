8308ba2ce93aa8f0b42508ae3ea9592a38224006
louaylh10 <hcinlouay@gmail.com>
2025-03-02 11:32:56 +0100

    blank screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './Authcontext'; // Assurez-vous que le chemin est correct
-
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Nouvelle méthode en React 18
+root.render(
   <React.StrictMode>
     <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </HashRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
