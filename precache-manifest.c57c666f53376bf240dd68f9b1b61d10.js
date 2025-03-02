8a435064f1254f930713417172efea80cfe5a9ee
louaylh10 <hcinlouay@gmail.com>
2025-03-02 13:10:18 +0100

    Updates

---
+++ precache-manifest.c57c666f53376bf240dd68f9b1b61d10.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b679f660bbe79be291337bd9a9c791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc7dbfc6d713204b2f7",
+    "url": "/static/css/2.c66d78bc.chunk.css"
+  },
+  {
+    "revision": "700f3ca1890f68038d04",
+    "url": "/static/css/main.22d54adb.chunk.css"
+  },
+  {
+    "revision": "5dc7dbfc6d713204b2f7",
+    "url": "/static/js/2.b81efa0b.chunk.js"
+  },
+  {
+    "revision": "475a116d3c88af66225e7c03a493ad5f",
+    "url": "/static/js/2.b81efa0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700f3ca1890f68038d04",
+    "url": "/static/js/main.706ee572.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "cb02ed21772b8f673f8b64f33cd414e9",
+    "url": "/static/media/448948920_2351813105024380_8089309549687949757_n.cb02ed21.jpg"
+  },
+  {
+    "revision": "00b0f8d1e1d8a309862a7893191ed505",
+    "url": "/static/media/448950463_1195122918309938_6478207158724499985_n.00b0f8d1.jpg"
+  },
+  {
+    "revision": "344a7b4a0c98289af7e5809571c5eba8",
+    "url": "/static/media/449176808_2055772204816609_4323812945297030382_n.344a7b4a.jpg"
+  },
+  {
+    "revision": "e3f4adbb2fe37b4ca1b8a3580159db7c",
+    "url": "/static/media/449626188_444195275239702_4730601301765446573_n.e3f4adbb.jpg"
+  },
+  {
+    "revision": "d2e721a4981c3fd136d8e4ea592223bb",
+    "url": "/static/media/449665207_783344360307700_5871136384043032416_n.d2e721a4.jpg"
+  },
+  {
+    "revision": "b4891719b36d6c9b228c22f2ae9c0163",
+    "url": "/static/media/Cheminée 018.b4891719.jpg"
+  },
+  {
+    "revision": "60506e766a360497229f59ebc7be974a",
+    "url": "/static/media/Logo-norme-ISO-14001.60506e76.png"
+  },
+  {
+    "revision": "badaea5f09291b57e66f55475a30fac9",
+    "url": "/static/media/Logo-norme-ISO-45001.badaea5f.png"
+  },
+  {
+    "revision": "15d54d142da2f2d6f2e90ed1d55121af",
+    "url": "/static/media/fa-brands-400.15d54d14.ttf"
+  },
+  {
+    "revision": "cbcf42b2e9228a8f5bad42717d8a88db",
+    "url": "/static/media/fa-brands-400.cbcf42b2.woff2"
+  },
+  {
+    "revision": "262525e2081311609d1fdab966c82bfc",
+    "url": "/static/media/fa-regular-400.262525e2.ttf"
+  },
+  {
+    "revision": "89672701a5874b80be27649e0494e354",
+    "url": "/static/media/fa-regular-400.89672701.woff2"
+  },
+  {
+    "revision": "269f971cec0d5dc864fe9ae080b19e23",
+    "url": "/static/media/fa-solid-900.269f971c.ttf"
+  },
+  {
+    "revision": "4a6591ab5460ae5cbff1ecbd6e52193a",
+    "url": "/static/media/fa-solid-900.4a6591ab.woff2"
+  },
+  {
+    "revision": "4ed293ceaca9b5b2d9cd74a477963fae",
+    "url": "/static/media/fa-v4compatibility.4ed293ce.ttf"
+  },
+  {
+    "revision": "a772da7bff216cd36214bc44165bba3e",
+    "url": "/static/media/fa-v4compatibility.a772da7b.woff2"
+  },
+  {
+    "revision": "131af536c238217dbad1de6a4a778506",
+    "url": "/static/media/industrial.131af536.png"
+  },
+  {
+    "revision": "f566f414aa1d3cbdf44b20901ee18682",
+    "url": "/static/media/logo_original.f566f414.png"
+  },
+  {
+    "revision": "1a71684701588761fa20c66579e57cc8",
+    "url": "/static/media/maison.1a716847.png"
+  },
+  {
+    "revision": "0d412faccf9d4fb292615b2ec520e587",
+    "url": "/static/media/ouvrier-dans-usine.0d412fac.jpg"
+  },
+  {
+    "revision": "8b80f6e72a343ad9390035ffda518d64",
+    "url": "/static/media/personne-combinaison-protection-travaillant-dans-centrale-nucleaire.8b80f6e7.jpg"
+  },
+  {
+    "revision": "f27e3f56d80a48c3a4832986da5353d9",
+    "url": "/static/media/png-transparent-organization-iso-9000-iso-9001-2015-certification-iso-9001-text-trademark-logo-thumbnail.f27e3f56.png"
+  },
+  {
+    "revision": "3593bd5792564844cd8d2f636964c83b",
+    "url": "/static/media/portrait-ingenieur-chantier-pendant-heures-travail.3593bd57.jpg"
+  },
+  {
+    "revision": "28c736345c19380f1e11cc9bb2a110b1",
+    "url": "/static/media/portrait-jeune-travailleur-dans-casque-dans-grande-usine-metallurgie.28c73634.jpg"
+  },
+  {
+    "revision": "10284267ce8291b7ed95633bb7c042f4",
+    "url": "/static/media/reservoir.10284267.png"
+  },
+  {
+    "revision": "accd2e7d8ff519da3307a78087600b05",
+    "url": "/static/media/tuyauterie.accd2e7d.png"
+  }
+]);
