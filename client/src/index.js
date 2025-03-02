fcd2652ef23ae58cbd1748a6171e84b1e9684d55
louaylh10 <hcinlouay@gmail.com>
2025-03-02 11:30:56 +0100

    blank screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './Authcontext'; // Assurez-vous que le chemin est correct
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
